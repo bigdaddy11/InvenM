8fc4b761af49a407e656372c58a0e2fff218bee0
Jae Hyun Heo <zidir0070@naver.com>
2024-11-15 17:06:21 +0900

    서버 포트변경

--- src/pages/common/api.js
+++ src/pages/common/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',  // 기본 서버 URL
+    baseURL: 'http://localhost:9090',  // 기본 서버 URL
     timeout: 5000,  // 요청 제한 시간 설정 (선택 사항)
     headers: { 'Content-Type': 'application/json' }  // 공통 헤더 설정 (필요에 따라)
 });
