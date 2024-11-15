06c3fcd4dd5578492fde0540d3e5b5c739125b2a
Jae Hyun Heo <zidir0070@naver.com>
2024-11-15 22:22:31 +0900

    개발/운영 API 호출부분 개선

--- src/pages/common/api.js
+++ src/pages/common/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:9090',  // 기본 서버 URL
+    //baseURL: 'http://localhost:9090',  // 기본 서버 URL
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 5000,  // 요청 제한 시간 설정 (선택 사항)
     headers: { 'Content-Type': 'application/json' }  // 공통 헤더 설정 (필요에 따라)
 });
